f71ab36c6064e56fbf07165f5571020688410b2e
jp-mariano <juanpaolo.mariano28@gmail.com>
2022-12-20 18:34:17 +0800

    Updated README to include modules used

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from gtts import gTTS
+from PyPDF2 import PdfReader
 
 # For testing purposes, we need to hear the voice and play the mp3 file
 import os
 
-tts = gTTS("Hello, this is my normal way of speaking!", lang="en")
-tts.save("./mp3_files/test.mp3")
 
-# Plays the mp3 file
-os.system("afplay ./mp3_files/test.mp3")
+
+# tts = gTTS("Hello, this is my normal way of speaking!", lang="en")
+# tts.save("./mp3_files/test.mp3")
+
+# # Plays the mp3 file
+# os.system("afplay ./mp3_files/test.mp3")
