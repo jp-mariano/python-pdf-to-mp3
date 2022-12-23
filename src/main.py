f2a28adf8dd1fe39fee1759479de783e0e802c58
jp-mariano <juanpaolo.mariano28@gmail.com>
2022-12-23 14:19:40 +0800

    Added file name extraction and using same file name with the mp3 file

--- src/main.py
+++ src/main.py
@@ -2,12 +2,17 @@ from gtts import gTTS
 from PyPDF2 import PdfReader
 
 # For testing purposes, we need to hear the voice and play the mp3 file
+# Also used to fetch the PDF file name
 import os
 
 try:
+	# Input the PDF document's file path below (replace "test.pdf" with your own file)
+	pdf_file_directory = "./pdf_files/test.pdf"
+	file_name = os.path.basename(pdf_file_directory).split(".")[0]
+
 	# Initializing the PdfReader module
 	text = ""
-	reader = PdfReader("./pdf_files/test.pdf") # TODO: dynamically capture the file name
+	reader = PdfReader(pdf_file_directory)
 	number_of_pages = len(reader.pages)
 
 	# Extracting the text for each page
@@ -21,10 +26,12 @@ try:
 	# Initializing the Google Text-to-Speech module
 	print("Converting...")
 	tts = gTTS(text, lang="en")
-	tts.save("./mp3_files/test.mp3") # Apply the TODO here too
+
+	mp3_file_directory = f"./mp3_files/{ file_name }.mp3"
+	tts.save(mp3_file_directory)
 	print("We now have an mp3 file!")
 
-	# Plays the mp3 file
-	os.system("afplay ./mp3_files/test.mp3")
+	# Plays the mp3 file (uncomment when testing)
+	# os.system(f"afplay { mp3_file_directory }")
 except Exception as e:
 	raise e
