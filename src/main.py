05a465a5b63dd74ac5aa229dd49ae90543149f6e
jp-mariano <juanpaolo.mariano28@gmail.com>
2022-12-23 13:47:04 +0800

    Added try-except block

--- src/main.py
+++ src/main.py
@@ -4,26 +4,27 @@ from PyPDF2 import PdfReader
 # For testing purposes, we need to hear the voice and play the mp3 file
 import os
 
-# TODO: use a try-except block
+try:
+	# Initializing the PdfReader module
+	text = ""
+	reader = PdfReader("./pdf_files/test.pdf") # TODO: dynamically capture the file name
+	number_of_pages = len(reader.pages)
 
-# Initializing the PdfReader module
-text = ""
-reader = PdfReader("./pdf_files/test.pdf") # TODO: dynamically capture the file name
-number_of_pages = len(reader.pages)
+	# Extracting the text for each page
+	print("Initiating text extraction...")
+	for page_num in range(number_of_pages):
+		page = reader.pages[page_num]
+		raw_text = page.extract_text()
+		text += raw_text.strip().replace("\n", " ")
+		print(f"Finished extracting page { page_num + 1 } of { number_of_pages } pages.")
 
-# Extracting the text for each page
-print("Initiating text extraction...")
-for page_num in range(number_of_pages):
-	page = reader.pages[page_num]
-	raw_text = page.extract_text()
-	text += raw_text.strip().replace("\n", " ")
-	print(f"Finished extracting page { page_num + 1 } of { number_of_pages } pages.")
+	# Initializing the Google Text-to-Speech module
+	print("Converting...")
+	tts = gTTS(text, lang="en")
+	tts.save("./mp3_files/test.mp3") # Apply the TODO here too
+	print("We now have an mp3 file!")
 
-# Initializing the Google Text-to-Speech module
-print("Converting...")
-tts = gTTS(text, lang="en")
-tts.save("./mp3_files/test.mp3") # Apply the TODO here too
-print("We now have an mp3 file!")
-
-# Plays the mp3 file
-os.system("afplay ./mp3_files/test.mp3")
+	# Plays the mp3 file
+	os.system("afplay ./mp3_files/test.mp3")
+except Exception as e:
+	raise e
